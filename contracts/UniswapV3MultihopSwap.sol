f86f01b1197360815efe629e86c4b348872c432e
Hwi <84950295+qhwi@users.noreply.github.com>
2024-06-03 02:08:02 +0700

    Create UniswapV3MultihopSwap.sol

---
+++ contracts/UniswapV3MultihopSwap.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity = 0.7.6;
+pragma abicoder v2;
+
+import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import '@uniswap/v3-core/contracts/libraries/LowGasSafeMath.sol';
+
+contract UniswapV3MultihopSwap {
+    using LowGasSafeMath for uint256;
+    using LowGasSafeMath for int256;
+    ISwapRouter public immutable swapRouter;
+
+    // Swaps WETH9/DAI/USDC
+    address public USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    constructor(ISwapRouter _swapRouter, address _USDC, address _DAI, address _WETH9) {
+        swapRouter = _swapRouter;
+        USDC = _USDC;
+        DAI = _DAI;
+        WETH9 = _WETH9;
+    }
+
+    uint256 private constant MAX_INT =
+        115792089237316195423570985008687907853269984665640564039457584007913129639935;
+
+    function swapExactInputMultihop(uint256 amountIn) external returns (uint256 amountOut) {
+        TransferHelper.safeTransferFrom(WETH9, msg.sender, address(this), MAX_INT);
+        TransferHelper.safeApprove(WETH9, address(swapRouter), MAX_INT);
+
+        ISwapRouter.ExactInputParams memory params =
+            ISwapRouter.ExactInputParams({
+                path: abi.encodePacked(WETH9, uint24(3000), DAI, uint24(100), USDC),
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountIn: amountIn,
+                amountOutMinimum: 0
+            });
+
+        amountOut = swapRouter.exactInput(params);
+    }
+
+    function swapExactOutputMultihop(uint256 amountOut, uint256 amountInMaximum) external returns (uint256 amountIn) {
+        TransferHelper.safeTransferFrom(WETH9, msg.sender, address(this), amountInMaximum);
+        TransferHelper.safeApprove(WETH9, address(swapRouter), amountInMaximum);
+
+        ISwapRouter.ExactOutputParams memory params =
+            ISwapRouter.ExactOutputParams({
+                path: abi.encodePacked(USDC, uint24(100), DAI, uint24(3000), WETH9),
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountOut: amountOut,
+                amountInMaximum: amountInMaximum
+            });
+
+        amountIn = swapRouter.exactOutput(params);
+
+        if (amountIn < amountInMaximum) {
+            TransferHelper.safeApprove(WETH9, address(swapRouter), 0);
+            TransferHelper.safeTransferFrom(WETH9, address(this), msg.sender, amountInMaximum - amountIn);
+        }
+    }
+}
