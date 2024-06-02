b6b03ea27429191d9b8c19b57554d2b8339e8a99
Hwi <84950295+qhwi@users.noreply.github.com>
2024-06-02 18:29:33 +0700

    Update hardhat.config.js
    check for existing variable and delete

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,8 +3,32 @@ const fs = require("fs");
 if (!fs.existsSync(".env")) {
   fs.writeFileSync(".env", "");
 }
+dotenv = require("dotenv")
+dotenv.config();
 
-require("dotenv").config();
+// Replace old variables with "null" in .env if they exist
+const envVariables = [
+  "WETH_ADDR",
+  "FACTORY_ADDR",
+  "SWAP_ROUTER_ADDR",
+  "NFT_DESCRIPTOR_ADDR",
+  "POSITION_DESCRIPTOR_ADDR",
+  "POSITION_MANAGER_ADDR",
+  "TETHER_ADDR",
+  "USDC_ADDR"
+];
+
+const envConfig = dotenv.parse(fs.readFileSync('.env'))
+envVariables.forEach((evar) => {
+  if (envConfig[evar]) {
+    delete envConfig[evar];
+  }
+});
+const env = Object.entries(envConfig).map(
+  ([key, value]) => `${key} = ${value}`
+).join('\n');
+fs.writeFileSync('.env', env);
+
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
